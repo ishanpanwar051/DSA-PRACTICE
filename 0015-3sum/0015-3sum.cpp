b4b45e47dab9607d4b7322e0cf795eb4c748469c
ishanpanwar051 <ishanpanwar051@gmail.com>
2026-07-06 18:16:40 +0530

    Time: 39 ms (95.07%), Space: 29 MB (73.39%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -4,38 +4,38 @@ public:
         sort(nums.begin(),nums.end());
 
         vector<vector<int>>ans;
-
         int n =nums.size();
 
-        for(int i=0;i<n-2;i++){
-            if(i>0 && nums[i]== nums[i-1])continue;
-            int left =i+1;
-            int right =n-1;
 
-            while(left<right){
-                int sum = nums[i]+nums[left]+nums[right];
+        for(int  i=0;i<n-2;i++){
+if(i>0 && nums[i] == nums[i-1]) continue;
 
-                if(sum==0){
-                    ans.push_back({nums[i],nums[left],nums[right]});
+int left =i+1;
+int right = n-1;
 
-                    left++;
-                    right--;
+while(left<right){
+    int sum = nums[i] +nums[left] +nums[right];
 
-                    while(left<right && nums[left] == nums[left-1])
-                    left++;
-                
-                 while(left<right && nums[right] == nums[right+1])
-                    right--;
-                }
-                else if(sum<0){
-                    left++;
+    if(sum == 0){
+        ans.push_back({nums[i],nums[left],nums[right]});
 
-                }
-                else{
-                    right--;
-                }
-            }
-        
+        left++;
+        right--;
+
+        while(left<right && nums[left] == nums[left-1])
+        left++;
+
+        while(left<right && nums[right] == nums[right+1]) right--;
+    }
+    else if(sum<0) {
+        left++;
+
+    }
+    else{
+        right--;
+    }
+}
+
         }
         return ans;
     }
