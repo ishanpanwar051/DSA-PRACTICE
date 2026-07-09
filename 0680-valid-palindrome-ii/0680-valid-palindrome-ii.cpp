557566bef577885b57a91dc11e9b6d8dbe4a7aa8
ishanpanwar051 <ishanpanwar051@gmail.com>
2026-07-09 15:29:59 +0530

    Time: 5 ms (40.1%), Space: 26.7 MB (15.25%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-
-bool ispalidorome(string&s , int left ,int right){
-
+bool ispalidrome(string s,int left,int right){
+  
     while(left<right){
-        if(s[left] != s[right]) return false;
-        left++;
-        right--;
-    }
-    return true;
-}
-
-
-    bool validPalindrome(string s) {
-        int left =0;
-        int right = s.size()-1;
-
-        while(left<right){
-            if(s[left] == s[right]){
+if(s[left]!=s[right])return false;
 left++;
 right--;
+
+    }
+    return true;
+    }
+    bool validPalindrome(string s) {
+        int left =0;
+        int right =s.size()-1;
+        while(left<right){
+            if(s[left] == s[right]){
+                left++;
+                right--;
             }
             else{
-                return ispalidorome(s,left+1,right) || 
-                ispalidorome(s,left,right-1);
+                return ispalidrome(s,left+1,right) || ispalidrome(s,left,right-1);
             }
+           
         }
-        return true;
+         return true;
     }
 };
