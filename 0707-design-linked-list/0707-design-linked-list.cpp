2bed0c3fc538810b8b01b1af21d4bf41e0c28541
ishanpanwar051 <ishanpanwar051@gmail.com>
2026-09-08 13:17:42 +0530

    Time: 23 ms (5.23%), Space: 25.6 MB (53.82%) - LeetHub

--- 0707-design-linked-list/0707-design-linked-list.cpp
+++ 0707-design-linked-list/0707-design-linked-list.cpp
@@ -1,83 +1,103 @@
 class MyLinkedList {
 public:
-struct Node{
-    int val;
-    Node* next;
-    Node(int x){
-        val = x;
-        next = nullptr;
-    }
-};
 
-Node*dummy;
-int size;
+    struct Node {
+        int val;
+        Node* next;
 
+        Node(int x) {
+            val = x;
+            next = NULL;
+        }
+    };
+
+    Node* dummy;
+    int size;
+
+    // Constructor
     MyLinkedList() {
         dummy = new Node(0);
-        size =0;
+        size = 0;
     }
-    
-    int get(int index) {
-        if(index<0 || index>=size) return -1;
 
-        Node* curr =dummy->next;
-        while(index--){
+    // Get
+    int get(int index) {
+
+        if (index < 0 || index >= size)
+            return -1;
+
+        Node* curr = dummy->next;
+
+        for (int i = 0; i < index; i++) {
             curr = curr->next;
         }
+
         return curr->val;
     }
-    
+
+    // Add at Head
     void addAtHead(int val) {
-        addAtIndex(0,val);
-    }
-  
-    
-    void addAtTail(int val) {
-        addAtIndex(size,val);
-    }
-    
-    void addAtIndex(int index, int val) {
-        if(index<0) index =0;
 
-        if(index>size) return;
+        Node* newNode = new Node(val);
 
-        Node* prev= dummy;
-        while(index--){
-            prev = prev->next;
-        }
-        Node*node = new Node(val);
-
-        node->next = prev->next;
-        prev->next = node;
+        newNode->next = dummy->next;
+        dummy->next = newNode;
 
         size++;
     }
-    
-    void deleteAtIndex(int index) {
-        if(index <0 || index>=size) return;
 
+    // Add at Tail
+    void addAtTail(int val) {
 
-        Node*prev =dummy;
+        Node* curr = dummy;
 
-        while(index--){
-            prev = prev->next;
+        while (curr->next != NULL) {
+            curr = curr->next;
         }
 
-        Node*temp = prev->next;
-        prev->next =temp->next;
+        curr->next = new Node(val);
+
+        size++;
+    }
+
+    // Add at Index
+    void addAtIndex(int index, int val) {
+
+        if (index < 0 || index > size)
+            return;
+
+        Node* curr = dummy;
+
+        for (int i = 0; i < index; i++) {
+            curr = curr->next;
+        }
+
+        Node* newNode = new Node(val);
+
+        newNode->next = curr->next;
+        curr->next = newNode;
+
+        size++;
+    }
+
+    // Delete at Index
+    void deleteAtIndex(int index) {
+
+        if (index < 0 || index >= size)
+            return;
+
+        Node* curr = dummy;
+
+        for (int i = 0; i < index; i++) {
+            curr = curr->next;
+        }
+
+        Node* temp = curr->next;
+
+        curr->next = curr->next->next;
 
         delete temp;
 
         size--;
     }
 };
-
-/**
- * Your MyLinkedList object will be instantiated and called as such:
- * MyLinkedList* obj = new MyLinkedList();
- * int param_1 = obj->get(index);
- * obj->addAtHead(val);
- * obj->addAtTail(val);
- * obj->addAtIndex(index,val);
- * obj->deleteAtIndex(index);
- */
