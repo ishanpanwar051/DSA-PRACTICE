27617e56da166310fad2972862577f9f8609aaeb
ishanpanwar051 <ishanpanwar051@gmail.com>
2026-09-23 11:44:12 +0530

    Time: 0 ms (100%), Space: 20.9 MB (88.5%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -1,39 +1,31 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
+    vector<vector<int>> ans;
+    vector<int> path;
 
-vector<vector<int>>ans;
-vector<int>path;
+    void dfs(TreeNode* root, int targetSum) {
+        if(root == nullptr)
+            return;
 
+        path.push_back(root->val);
+        targetSum -= root->val;
 
-void dfs( TreeNode* root , int target){
-    if(root == NULL) return;
+        if(root->left == nullptr && root->right == nullptr) {
+            if(targetSum == 0)
+                ans.push_back(path);
 
-    path.push_back(root->val);
+            path.pop_back();
+            return;
+        }
 
-    target -= root->val;
+        dfs(root->left, targetSum);
+        dfs(root->right, targetSum);
 
-    if(root->left == NULL && root->right == NULL && target == 0){
-        ans.push_back(path);
+        path.pop_back();
     }
-    dfs(root->left,target);
-    dfs(root->right,target);
 
-    path.pop_back();
-}
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        dfs(root,targetSum);
+        dfs(root, targetSum);
         return ans;
-
     }
 };
